2e7b24d79417be55d19b85ce132c77be3ece51a5
ajwipo <tropaguararia28@gmail.com>
2022-06-04 12:27:01 -0500

    grinding settings

--- demo-project/TileMap.gd
+++ demo-project/TileMap.gd
@@ -8,7 +8,7 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$LiquidSim.set_liquid(15, -1, 5.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,5 +19,5 @@ func _ready():
 func _input(event):
 	# Mouse in viewport coordinates
 	if event is InputEventMouseButton:
-		var wpos = world_to_map(get_global_mouse_position())
-		$LiquidSim.add_liquid(wpos.x, wpos.y, 10.0)
+		var wpos = world_to_map(get_global_mouse_position())		
+		$LiquidSim.add_liquid(wpos.x, wpos.y, 5.0)
